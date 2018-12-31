53aee4d53f619f194b82357498918b5f44b60dcb
tristan <tristanbgiles@gmail.com>
2018-12-31 13:46:12 -0800

    Temporary state pattern change

--- app.py
+++ app.py
@@ -19,28 +19,6 @@ app = Flask(__name__) #App is now an instance of Flask.
 
 @app.route("/start", methods=["POST"])
 def start():
-    print("Starting start method")
-    data = request.get_json()
-
-    global height
-    height = data.get("height")
-    global width
-    width = data.get("weight")
-    global game_id
-    game_id = data.get("game_id")
-
-    # Game States
-    global defend
-    defend = State_Defend()
-    global attack
-    attack = State_Attack()
-    global grow
-    grow = State_Grow()
-
-    # Current State
-    global state
-    state = defend
-
     return jsonify( color = "#E0FFFF", secondary_color = "#000000", name = "FeistySnake", taunt = "Mess with Snekko, better run like hecko", head_type = "shades", tail_type = "freckled", head_url = "https://todaysmusings.files.wordpress.com/2008/05/raccoon.jpg?w=229&h=300")
 
 @app.route("/move", methods=["POST"])
@@ -49,6 +27,13 @@ def move():
 
     #NOTE grid_data[0] = move_grid // grid_data[1] = food_grid
     grid_data = Setup().grid_setup(data) #(food, width, height, snakes, myID)
+
+    # Game States
+    defend = State_Defend()
+    attack = State_Attack()
+    grow = State_Grow()
+
+    state = defend
 
     #NOTE Get the next move based on the pellet
     next_move = state.get_move(grid_data, data)
