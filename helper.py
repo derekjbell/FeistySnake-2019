fb7b6ead9be8ace1b5d8f829ddfc08a991d0a76a
tbgiles <tristanbgiles@gmail.com>
2019-02-18 14:58:40 -0800

    Prints off potential moves retrieved by get neighbours

--- helper.py
+++ helper.py
@@ -43,7 +43,12 @@ class Helper():
     def get_neighbors(self, node, lines, height, width):
         (x, y) = node #changed from x, y
         # return[(nx, ny) for nx, ny in[(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)] if 0 <= nx < width and 0 <= ny < height and lines[ny][nx] == 1]
-        return self.sort_options_fill([(nx, ny) for nx, ny in[(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)] if 0 <= nx < width and 0 <= ny < height and lines[ny][nx] == 1], lines).reverse()
+        potential_moves = [(nx, ny) for nx, ny in[(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)] if 0 <= nx < width and 0 <= ny < height and lines[ny][nx] == 1]
+        print(potential_moves)
+        if len(potential_moves) > 0:
+            return self.sort_options_fill(potential_moves, lines).reverse()
+        else:
+            return None
 
     def get_last_resort(self, node, lines, height, width):
         (x, y) = node #changed from x, y
