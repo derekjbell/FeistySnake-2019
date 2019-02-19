1b3ccea01de9cfdb6fcdad3d08cd60173cbbfbb0
tbgiles <tristanbgiles@gmail.com>
2019-02-18 16:16:05 -0800

    Added floodfill to last resort as well

--- helper.py
+++ helper.py
@@ -53,7 +53,14 @@ class Helper():
 
     def get_last_resort(self, node, lines, height, width):
         (x, y) = node #changed from x, y
-        return[(nx, ny) for nx, ny in[(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)] if 0 <= nx < width and 0 <= ny < height and (lines[ny][nx] == 1 or lines[ny][nx] == -1)]
+        # return[(nx, ny) for nx, ny in[(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)] if 0 <= nx < width and 0 <= ny < height and lines[ny][nx] == 1]
+        potential_moves = [(nx, ny) for nx, ny in[(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)] if 0 <= nx < width and 0 <= ny < height and lines[ny][nx] != 0]
+        print("Potential moves:")
+        print(potential_moves)
+        if len(potential_moves) > 0:
+            return self.sort_options_fill(potential_moves, lines)
+        else:
+            return None
 
     def sort_options_fill(self, list, map):
         filler = floodfill.FloodFill([map,[]])
